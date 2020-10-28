9ac27840a9e699b8451c25ef4e1285dd93f52801
Sebastian Zivota <sebastian.zivota@mailbox.org>
2020-10-28 19:10:29 +0100

    Minimal main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting bot …");
+
+    let bot = Bot::from_env();
 }
