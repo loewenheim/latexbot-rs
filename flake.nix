d91ba6b508da21c6464237487e8a3dd18f0f6ba0
Sebastian Zivota <sebastian.zivota@mailbox.org>
2020-10-28 19:09:08 +0100

    Added pkgconfig and glibc to flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
     # setup the devShell for x86_64-linux.
     devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux;
       mkShell {
+        nativeBuildInputs = [ pkgconfig ];
         buildInputs = [
+          glibc
           openssl
         ];
 
